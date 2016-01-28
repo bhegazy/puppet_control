918ae5532ec1a72e65d98c5b15930f7d7a879fa6
Bill Hegazy <bill.hegazy@gmail.com>
2016-01-28 10:51:04 +0800

    Update postrun.sh

--- postrun.sh
+++ postrun.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -x
+#Testing vcsrepo
 #/bin/gzip -vf /opt/puppetlabs/server/data/puppetserver/r10k/git@github.com-bhegazy-pp-control.git/objects/pack/*
 pushd "/opt/puppetlabs/server/data/puppetserver/r10k/git@github.com-bhegazy-pp-control.git/objects/pack/"
 ls -1 | find . -printf "%T@ %Tc %p\n" | sort -n -r | tail -n +6 | xargs /bin/gzip -vf > /dev/null 2>&1
